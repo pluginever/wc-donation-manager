a14f02031e5001e0f424523c73153b75f48f585e
Kawsar Ahmed <54880093+wpfresher@users.noreply.github.com>
2023-10-16 17:37:51 +0600

    Create WC_Product_Donation.php
    Added WC_Product_Donation to include as a plugin services.

---
+++ src/WC_Product_Donation.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace WooCommerceDonationManager;
+
+defined( 'ABSPATH' ) || exit;
+
+//if( class_exists( '\WC_Product_Simple' ) ){
+//	require_once( WC()->plugin_path() . '/includes/class-wc-product-simple.php' );
+//}
+/**
+ * Class ProductTypes.
+ *›
+ * Extends WC product simple type for Donation
+ *
+ * @package WooCommerceDonationManager
+ * @since 1.0.0
+ */
+class WC_Product_Donation extends \WC_Product_Simple {
+
+	/**
+	 * Initialize simple product.
+	 *
+	 * @param \WC_Product_Simple|int $product Product instance or ID.
+	 */
+
+	/**
+	 * Return the product type
+	 * @return string
+	 */
+	public function get_type() {
+		return 'donation';
+	}
+
+	/**
+	 * Returns the product's active price.
+	 *
+	 * @param  string $context What the value is for. Valid values are view and edit.
+	 * @return string price
+	 */
+}
