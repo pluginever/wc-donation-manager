e7a518e55fba8e4954e882cfcf9049994ba63639
Kawsar Ahmed <54880093+wpfresher@users.noreply.github.com>
2023-10-16 17:34:10 +0600

    Update Metaboxes.php
    Added product metaboxes

--- src/Admin/Metaboxes.php
+++ src/Admin/Metaboxes.php
@@ -17,51 +17,73 @@ class Metaboxes {
 	 * @since 1.0.0
 	 */
 	public function __construct() {
-		add_filter( 'product_type_options', array( __CLASS__, 'product_type_option' ), 11 );
-		add_filter( 'woocommerce_product_data_tabs', array( __CLASS__, 'product_data_tab' ) );
-
-//		add_action( 'woocommerce_product_data_panels', array( __CLASS__, 'product_write_panel' ) );
-//		add_filter( 'woocommerce_process_product_meta', array( __CLASS__, 'product_save_data' ) );
-//		add_action( 'woocommerce_product_after_variable_attributes', array( __CLASS__, 'variable_product_content' ), 10, 3 );
+		add_filter( 'product_type_selector', array( __CLASS__, 'add_type' ) );
+		add_filter( 'woocommerce_product_data_tabs', array( __CLASS__, 'product_data_tab' ), 10, 1 );
+		add_filter('woocommerce_product_options_general_product_data', array( __CLASS__, 'general_product_data' ) );
 	}
 
 	/**
-	 * Add "Donation" as a product type option
-	 * Only if this option is enabled one can donate to the product
+	 * Add "Donation" as a product type selector.
+	 * Only if this option is selected one can donate to the product.
 	 *
-	 * @param $product_type_options
+	 * @param array $product_type array of product types.
 	 *
 	 * @version 1.0.0
-	 * @return array
+	 * @return array array of product types.
 	 */
-	public static function product_type_option( $product_type_options ): array {
-		$product_type_options[ 'wcdm_donation' ] = [
-			'id'            => 'wcdm_donation',
-			'wrapper_class' => 'show_if_simple show_if_variable show_if_grouped',
-			'label'         => __( 'Donation', 'wc-donation-manager' ),
-			'description'   => __( 'This product will only be used for donation if activated', 'wc-donation-manager' ),
-			'default'       => 'on',
-		];
-
-		return $product_type_options;
+	public static function add_type( $product_type ) {
+		$product_type['donation'] = __( 'Donation', 'wc-donation-manager' );
+		return $product_type;
 	}
 
 	/**
 	 * Add donation tab on product edit page.
+	 * Hide all except the General and Advanced product data tabs for Donation products.
 	 *
 	 * @param array $tabs Product data tabs.
 	 *
 	 * @version 1.0.0
-	 * @return array
+	 * @return array array of product data tabs.
 	 */
 	public static function product_data_tab( array $tabs = array() ): array {
-		$wcdm_tab =  array(
+		foreach ($tabs as $tabId => $tabData) {
+			if ($tabId != 'general' && $tabId != 'advanced') {
+				$tabs[$tabId]['class'][] = 'hide_if_donation';
+			}
+		}
+
+		$tabs[] =  array(
 			'label'    => __( 'Donation Manager', 'wc-donation-manager' ),
 			'target'   => 'wcdm_form_data',
-			'class'    => 'show_if_donation_active hidden wcdm_donation hide_if_external',
+			'class'    => 'show_if_donation hidden hide_if_external',
 			'priority' => 11,
 		);
-		$tabs[] = $wcdm_tab;
 		return $tabs;
+	}
+
+	/**
+	 * Add fields to the General product data tab.
+	 *
+	 * @version 1.0.0
+	 * @return void
+	 */
+	public static function general_product_data() {
+		echo '<div class="options_group show_if_donation">';
+		woocommerce_wp_text_input( array(
+			'id' => 'donation_default_amount',
+			'label' => esc_html__('Default amount',
+				'donations-for-woocommerce'),
+			'value' => get_post_meta( get_the_ID(), '_price', true ),
+			'data_type' => 'price'
+		));
+		$donationAmountIncrement = get_post_meta( get_the_ID(), '_donation_amount_increment', true);
+		woocommerce_wp_text_input( array(
+			'id' => 'donation_amount_increment',
+			'label' => esc_html__('Amount increment',
+				'donations-for-woocommerce'),
+			'value' => ( empty( $donationAmountIncrement ) ? 0.01 : $donationAmountIncrement ),
+			'data_type' => 'decimal'
+		));
+		echo '</div>';
 	}
 }
