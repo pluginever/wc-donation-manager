776c2d4e1ffe8dc30c7dad20a82a63f8c12eaab7
Kawsar Ahmed <54880093+wpfresher@users.noreply.github.com>
2023-10-16 17:36:42 +0600

    Create class-wc-product-donation.php
    Added WC_Product_Donation class, Extended from WC_product_Simple product class

---
+++ src/class-wc-product-donation.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Advanced Product Type
+ */
+class WC_Product_Donation extends WC_Product_Simple {
+	/**
+	 * Initialize simple product.
+	 *
+	 * @param WC_Product_Simple|int $product Product instance or ID.
+	 *
+	 * @since 1.0.0
+	 */
+	public function __construct( $product = 0 ) {
+		parent::__construct( $product );
+	}
+
+	/**
+	 * Return the product type donation
+	 *
+	 * @since 1.0.0
+	 * @return string
+	 */
+	public function get_type() {
+		return 'donation';
+	}
+
+
+
+	/**
+	 * Get the add to url used mainly in loops.
+	 *
+	 * @return string
+	 */
+//	public function add_to_cart_url() {
+//		$url = $this->is_purchasable() && $this->is_in_stock() ? remove_query_arg(
+//			'added-to-cart',
+//			add_query_arg(
+//				array(
+//					'add-to-cart' => $this->get_id(),
+//				),
+//				( function_exists( 'is_feed' ) && is_feed() ) || ( function_exists( 'is_404' ) && is_404() ) ? $this->get_permalink() : ''
+//			)
+//		) : $this->get_permalink();
+//		return apply_filters( 'woocommerce_product_add_to_cart_url', $url, $this );
+//	}
+
+	/**
+	 * Get the add to cart button text.
+	 *
+	 * @return string
+	 */
+	public function add_to_cart_text() {
+		$text = $this->is_purchasable() && $this->is_in_stock() ? __( 'Donate Now', 'woocommerce' ) : __( 'Read more (Donate)', 'woocommerce' );
+
+		return apply_filters( 'woocommerce_product_add_to_cart_text', $text, $this );
+	}
+
+	/**
+	 * Get the add to cart button text description - used in aria tags.
+	 *
+	 * @since 3.3.0
+	 * @return string
+	 */
+	public function add_to_cart_description() {
+		/* translators: %s: Product title */
+		$text = $this->is_purchasable() && $this->is_in_stock() ? __( 'Add &ldquo;%s&rdquo; to your cart (Donate)', 'woocommerce' ) : __( 'Read more about &ldquo;%s&rdquo; (Donate)', 'woocommerce' );
+
+		return apply_filters( 'woocommerce_product_add_to_cart_description (Donate)', sprintf( $text, $this->get_name() ), $this );
+	}
+
+}
