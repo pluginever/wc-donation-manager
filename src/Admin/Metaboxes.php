a5944e4397bffce3c110e583e1f10bdbe5d36853
wpfresher <kawsar.ahmed198133@gmail.com>
2023-10-19 09:39:38 +0600

    Updated frontend Funtionality for donation product type. And also updated admin settings.

--- src/Admin/Metaboxes.php
+++ src/Admin/Metaboxes.php
@@ -76,23 +76,27 @@ class Metaboxes {
 		woocommerce_wp_text_input( array(
 			'id'          => 'wcdm_amount',
 			'label'       => __( 'Default amount', 'wc-donation-manager' ),
-			'description' => __( 'Enter the default amount for the donation.', 'wc-donation-manager' ),
+			'description' => __( 'Enter the default amount for the campaign.', 'wc-donation-manager' ),
 			'desc_tip'    => false,
 			'value'       => get_post_meta( get_the_ID(), '_price', true ),
 			'data_type'   => 'price',
 		) );
 		woocommerce_wp_text_input( array(
-			'id'        => 'wcdm_goal_amount',
-			'label'     => __( 'Goal amount', 'wc-donation-manager' ),
-			'value'     => get_post_meta( get_the_ID(), 'wcdm_goal_amount', true ),
-			'data_type' => 'price',
+			'id'          => 'wcdm_goal_amount',
+			'label'       => __( 'Goal amount', 'wc-donation-manager' ),
+			'description' => __( 'Enter the goal amount for the campaign.', 'wc-donation-manager' ),
+			'desc_tip'    => false,
+			'value'       => get_post_meta( get_the_ID(), 'wcdm_goal_amount', true ),
+			'data_type'   => 'price',
 		) );
 		$amount_increment = get_post_meta( get_the_ID(), 'wcdm_amount_increment_steps', true );
 		woocommerce_wp_text_input( array(
-			'id'        => 'wcdm_amount_increment_steps',
-			'label'     => __( 'Amount increment steps', 'wc-donation-manager' ),
-			'value'     => ( empty( $amount_increment ) ? 0.01 : $amount_increment ),
-			'data_type' => 'decimal',
+			'id'          => 'wcdm_amount_increment_steps',
+			'label'       => __( 'Amount increment steps', 'wc-donation-manager' ),
+			'description' => __( 'Enter the amount increment steps for the campaign amount field. This will applicable for increasing or decreasing amounts on the campaign page.', 'wc-donation-manager' ),
+			'desc_tip'    => false,
+			'value'       => ( empty( $amount_increment ) ? 0.01 : $amount_increment ),
+			'data_type'   => 'decimal',
 		) );
 		echo '</div>';
 	}
@@ -104,10 +108,13 @@ class Metaboxes {
 	 * @return void
 	 */
 	public static function product_data() {
-	echo '<div id="wcdm_tab_data" class="panel woocommerce_options_panel wcdm_tab_data_options"><div class="options_group show_if_donation">';
-		// TODO: Need to add metabox fields.
-	echo '</div></div>';
+		echo '<div id="wcdm_tab_data" class="panel woocommerce_options_panel wcdm_tab_data_options"><div class="options_group show_if_donation">';
 
+
+
+
+		// TODO: Need to add metabox fields.
+		echo '</div></div>';
 
 //		global $post;
 //		$product = wc_get_product( $post->ID );
