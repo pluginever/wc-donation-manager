62148b199b42b7f73828580dfed6b94e4df07e3f
wpfresher <kawsar.ahmed198133@gmail.com>
2023-10-23 00:57:27 +0600

    Updated email class to making custom email template for donation type only.

--- src/Emails/Emails.php
+++ src/Emails/Emails.php
@@ -26,8 +26,8 @@ class Emails {
 	/**
 	 * Sending custom and formatted email on change the order status.
 	 *
-	 * @param int $order_id Order ID.
-	 * @param \WC_Order $order Order object
+	 * @param int       $order_id Order ID.
+	 * @param \WC_Order $order Order object.
 	 *
 	 * @version 1.0.0
 	 * @return void
@@ -49,23 +49,14 @@ class Emails {
 		$mailer['WC_Email_Customer_Completed_Order']->trigger( $order_id );
 
 		// You have to use the email ID chosen above and also that $order->get_status() == "refused"
-
 	}
 
 	/**
-	 * @snippet       Add Text to Customer Processing Order Email
-	 * @how-to        Get CustomizeWoo.com FREE
-	 * @author        Rodolfo Melogli
-	 * @testedwith    Woo 4.6
-	 * @donate $9     https://businessbloomer.com/bloomer-armada/
-	 */
-
-	/**
-	 * Add custom text to the Customer Processing Order Email
+	 * Add custom text to the Customer Processing Order Email.
 	 *
-	 * @param \WC_Order $order Order object
-	 * @param bool $sent_to_admin Send email to admin.
-	 * @param string $plain_text Email plain text.
+	 * @param \WC_Order $order Order object.
+	 * @param bool      $sent_to_admin Send email to admin.
+	 * @param string    $plain_text Email plain text.
 	 * @param \WC_Email $email Email object.
 	 *
 	 * @version 1.0.0
@@ -73,17 +64,17 @@ class Emails {
 	 */
 	public static function add_content_specific_email( $order, $sent_to_admin, $plain_text, $email ) {
 
-//      Possible conditions for sending the emails.
-//		if ( $email->id == 'cancelled_order' ) {}
-//		if ( $email->id == 'customer_completed_order' ) {}
-//		if ( $email->id == 'customer_invoice' ) {}
-//		if ( $email->id == 'customer_new_account' ) {}
-//		if ( $email->id == 'customer_note' ) {}
-//		if ( $email->id == 'customer_on_hold_order' ) {}
-//		if ( $email->id == 'customer_refunded_order' ) {}
-//		if ( $email->id == 'customer_reset_password' ) {}
-//		if ( $email->id == 'failed_order' ) {}
-//		if ( $email->id == 'new_order' ) {}
+		// Possible conditions for sending the emails.
+		// if ( $email->id == 'cancelled_order' ) {}
+		// if ( $email->id == 'customer_completed_order' ) {}
+		// if ( $email->id == 'customer_invoice' ) {}
+		// if ( $email->id == 'customer_new_account' ) {}
+		// if ( $email->id == 'customer_note' ) {}
+		// if ( $email->id == 'customer_on_hold_order' ) {}
+		// if ( $email->id == 'customer_refunded_order' ) {}
+		// if ( $email->id == 'customer_reset_password' ) {}
+		// if ( $email->id == 'failed_order' ) {}
+		// if ( $email->id == 'new_order' ) {}
 
 		if ( $email->id == 'customer_processing_order' ) {
 			echo '<h2 class="email-upsell-title">Get 30% off</h2><p class="email-upsell-p">Thank you for making this purchase! Come back and use the code "<strong>SAVE30</strong>" to receive a 30% discount on your next purchase! Click here to continue shopping.</p>';
