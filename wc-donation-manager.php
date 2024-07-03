0335faf8cfd98d253cb6ac577380855d8737c1fe
kawsarahmedr <kawsar.ahmed198133@gmail.com>
2024-07-03 14:42:11 +0600

    Update header doc block

--- wc-donation-manager.php
+++ wc-donation-manager.php
@@ -2,18 +2,24 @@
 /**
  * Plugin Name:  WC Donation Manager
  * Description:  A powerful and user-friendly WordPress plugin designed to seamlessly integrate donation functionality into the WooCommerce platform. This plugin is the ultimate solution for effortlessly managing and receiving donations for a charitable organization, a non-profit, or a business looking to support a cause.
- * Version:      1.0.0
+ * Version:      1.0.1
  * Plugin URI:   https://pluginever.com/plugins/wc-donation-manager/
  * Author:       PluginEver
  * Author URI:   https://pluginever.com/
  * Text Domain:  wc-donation-manager
- * Domain Path: /languages/
- * Requires PHP: 5.6
- * WC requires at least: 3.0.0
- * WC tested up to: 7.1.0
+ * Domain Path: /languages
+ * License:      GPL v2 or later
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires PHP: 7.2
+ * Requires at least: 5.2
+ * WC requires at least: 5.0
+ * WC tested up to: 9.0
+ * Requires Plugins: woocommerce
  *
  * @package WooCommerceDonationManager
- *
+ */
+
+/**
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
